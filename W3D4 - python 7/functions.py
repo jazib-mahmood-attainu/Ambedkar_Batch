127ceb9a074ce9633240f82bb2d2c6a9dd618aac
jazib-sudo <jazib.prof@gmail.com>
2021-08-05 23:06:21 +0530

    class code W3D4

---
+++ W3D4 - python 7/functions.py
@@ -0,0 +1,83 @@
+"""
+Functions are constructors in every programming language that help us in:
+1. reudcing the duplicacy in code
+2. in making our code easy to understand
+3. wrapping our into smaller elements.
+
+syntax:
+def <function-name>():
+<tab>add the lines 
+<tab>i want to repeat 
+<tab>again and again
+
+
+how to use a function:
+
+function_name()
+
+
+q - 1 ) sum_of_cubes_till_5()
+q - 2 ) adding two numbers using function
+q - 3 ) printing name and batch of students
+"""
+# def greet():
+#     print("Hello")
+#     print("Welcome to this class")
+#     print("This is coming from function")
+
+
+
+
+# greet()
+# print("Someother work")
+# greet()
+# b = 6
+# c = 7
+# a = b+c
+# print(b)
+# greet()
+# def sum_of_cubes_till_5():
+#     n = 5
+#     sum = 0
+#     for i in range(1,n+1):
+#         sum += i**3
+#     print(sum)
+# sum_of_cubes_till_5()
+
+
+
+########################################
+
+# def add_two_nos(a,b): #a = 2, b = 3
+#     s = a+b
+#     return s
+    
+
+
+# t = add_two_nos(2,3)   #s = 5
+# print(t)
+
+
+
+# ##########################################
+# def greet(): 
+#     a = str(input("Enter your name")) #name
+#     b = str(input("Enter your batch")) #batch
+#     print("hi", a ,"Welcome to the class") 
+#     print("Enjoy the ",b,"batch Session by AttainU ") 
+
+# n = int(input("Enter number of students"))
+# for i in range(n):
+#     greet()
+
+# ##########################################
+# s = "attainu "*5
+# print(s,end="sdf") #stringstring
+############################################
+
+
+
+
+
+
+
